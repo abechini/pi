0cae4cd32692bf0f144c72c13e8e08857fe09cb9
aya1409 <aya.fathallah@esprit.tn>
2022-07-12 00:29:04 +0100

    pom and resources modifications

--- src/main/java/com/esprit/bankPi/chatBot/ChatController.java
+++ src/main/java/com/esprit/bankPi/chatBot/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-
+@Controller
 public class ChatController {
 	@MessageMapping("/chat.register")
 	@SendTo("/topic/public")
