1ca8305504d973887849f45db3a0c63510d06e45
aya1409 <aya.fathallah@esprit.tn>
2022-07-18 02:34:05 +0100

    change on reclamation id type to long and  add serch reclamation bu name

--- src/main/java/com/esprit/bankPi/model/Reclamation.java
+++ src/main/java/com/esprit/bankPi/model/Reclamation.java
@@ -14,34 +14,36 @@ import com.esprit.bankPi.chatBot.ChatMessage.MessageType;
 @Entity
 @Table(name="Reclamation")
 public class Reclamation {
+	private Long id;
+	private String solution;
+	public String Name;
+	private String Date;
 	 @Id
-		@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	 @Column(name = "id", unique = false, nullable = true, insertable = true, updatable = true)
-	    public int id;
-	    @Column(name = "Name", unique = false, nullable = true, insertable = true, updatable = true)
-		 public String Name;
-		 @Column(name = "solution", unique = false, nullable = true, insertable = true, updatable = true)
-		private String solution;
-		 @Column(name = "Date", unique = false, nullable = true, insertable = true, updatable = true)
-		private String Date;
-		public String getName() {
-			return Name;
-		}
-		public void setName(String name) {
-			Name = name;
-		}
-		public String getSolution() {
-			return solution;
-		}
-		public void setSolution(String solution) {
-			this.solution = solution;
-		}
-		public String getDate() {
-			return Date;
-		}
-		public void setDate(String date) {
-			Date = date;
-		}
-
-
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSolution() {
+		return solution;
+	}
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	
 }
