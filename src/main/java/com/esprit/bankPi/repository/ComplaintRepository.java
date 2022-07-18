7b2cebd614a530dedb61c6f48104572a6ddca16b
maissabn <maissa.benromdhane@esprit.tn>
2022-07-18 11:30:28 +0100

    New commit complaint

--- src/main/java/com/esprit/bankPi/repository/ComplaintRepository.java
+++ src/main/java/com/esprit/bankPi/repository/ComplaintRepository.java
@@ -1,11 +1,22 @@
 package com.esprit.bankPi.repository;
 
-import com.esprit.bankPi.data.Agency;
+
+import com.esprit.bankPi.data.Client;
 import com.esprit.bankPi.data.Complaint;
+import com.esprit.bankPi.enums.ComplaintState;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint,Long> {
-
+	@SuppressWarnings("unchecked")
+	public  Complaint save(Complaint complaint) ;
+	public List<Complaint> findByClient(Client client);
+	public  Complaint save(ComplaintState state) ;
+	public List<Complaint> findComplaintsByCurrentState(ComplaintState state) ;
+	
+	
 }
