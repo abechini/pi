f18973f2a53e80af2bf27a319697cbe06bde04c9
gfilaliEsprit <ghassen.filali1@esprit.tn>
2022-07-20 01:36:34 +0100

    Forum

---
+++ src/main/java/com/esprit/bankPi/data/Topic.java
@@ -0,0 +1,65 @@
+package com.esprit.bankPi.data;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Topic {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+	public List<Evaluation> getEvaluations() {
+		return evaluations;
+	}
+	public void setEvaluations(List<Evaluation> evaluations) {
+		this.evaluations = evaluations;
+	}
+	private String description;
+//	@JsonIgnore
+//	@ManyToOne
+//	private User user;
+	@OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
+	private List<Comment> comments;
+	@OneToMany(mappedBy = "topic")
+	private List<Evaluation> evaluations;
+}
